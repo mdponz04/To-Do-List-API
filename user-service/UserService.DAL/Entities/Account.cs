904fdf261d322647a3c14bcff948bbfbaa23361e
DuongDucManh <manhmanhd0402@gmail.com>
2026-03-27 10:16:33 +0700

    Create model

--- user-service/UserService.DAL/Entities/Account.cs
+++ user-service/UserService.DAL/Entities/Account.cs
@@ -1,6 +1,14 @@
-﻿namespace UserService.DAL.Entities
+﻿using UserService.DAL.Entities.BaseModelEntity;
+
+namespace UserService.DAL.Entities
 {
-    internal class Account
+    public class Account : BaseEntity
     {
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public virtual User? User { get; set; }
+        public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
 }
--- user-service/UserService.DAL/Entities/RefreshToken.cs
+++ user-service/UserService.DAL/Entities/RefreshToken.cs
@@ -2,11 +2,12 @@
 
 namespace UserService.DAL.Entities
 {
-    public class User : BaseEntity
+    public class RefreshToken : BaseEntity
     {
+        public string Token { get; set; } = string.Empty;
         public Guid AccountId { get; set; }
-        public string FullName { get; set; } = string.Empty;
-        public string? AvatarUrl { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
         public virtual Account? Account { get; set; }
     }
 }
